d04bb67b71aacc169da4ee7ceaac3b58907759d3
Nikolai Koudelia <nikoudel@gmail.com>
2020-05-31 20:45:11 +0000

    Add run-detached.sh

--- run-detached.sh
+++ run-detached.sh
@@ -2,9 +2,11 @@ sudo docker run \
   --name=openvpn \
   -v openvpn:/mnt/openvpn \
   -p 1194:1194/udp \
+  -p 5555:5555 \
   --network=vpn \
   --ip=172.20.108.2 \
   --cap-add=NET_ADMIN \
   --device=/dev/net/tun \
+  --restart="unless-stopped" \
   -d \
   nikoudel/openvpn sh /mnt/openvpn/start-vpn.sh
